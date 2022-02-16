9e8d87cb4d19c22e14ee83eaf62b4e60955d532d
imHyejinPark <qkrgpwls1010@gmail.com>
2022-02-16 22:51:46 +0900

    Update 추석트래픽.cpp
    숫자 다룰때 항상 자릿수 주의하기...^^
    문제도 꼼꼼히 읽어서 안해도 되는거 하지 말기~

--- Level 3/추석트래픽.cpp
+++ Level 3/추석트래픽.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <sstream>
 
 using namespace std;
@@ -11,24 +10,22 @@ int solution(vector<string> lines) {
 
     double* compTime = new double[len]; // 완료시각을 모두 더하여 sec 단위로 나타낸 값
     double* procTime = new double[len]; // 처리시간 
-    int hour, min, sec;
+    double hour, min, sec;
 
     for (int i = 0; i < len; i++) {
         lines[i][13] = lines[i][16] = ' '; // :를 공백으로 바꿔 시분초를 각각 나눠 저장함
         istringstream ss(lines[i]);
         string date;
         ss >> date >> hour >> min >> sec >> procTime[i];
-        compTime[i] = hour * 360.0 + min * 60.0 + sec;
+        compTime[i] = hour * 3600.0 + min * 60.0 + sec;
     }
 
-    // 완료시점이 빠른 순으로 정렬
-    sort(compTime, compTime + len);
     // 현재 인덱스의 뒷 요청들의 시작시간이 현재 인덱스의 완료시점 + 1초 보다 작은 것들의 개수를 구하기
     int max;
     for (int i = 0; i < len; i++) {
         max = 1;
         for (int j = i+1; j < len; j++) {
-            if (compTime[j] - procTime[i] < compTime[i] + 1) max++;
+            if (compTime[j] - procTime[j] < compTime[i] + 0.999) max++;
         }
         if (answer < max) answer = max;
     }
