2d57a616e121a4a901f143ecace75a13f4ff698e
imHyejinPark <qkrgpwls1010@gmail.com>
2022-03-07 23:15:53 +0900

    Update 더_맵게.cpp
    우선순위 큐는 참 조은거구나

--- Level 2/더_맵게.cpp
+++ Level 2/더_맵게.cpp
@@ -1,34 +1,33 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-
+#include <functional> 
+#include <queue>
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    int idx = 0;
-    int newFood = 0;
-    sort(scoville.begin(), scoville.end());
 
-    while (scoville[idx] < K) {
-        if (scoville.size() == idx + 1) return -1;
-        answer++;
-        newFood = scoville[idx] + 2 * scoville[idx + 1];
-        if (scoville.size() == idx + 2) {
-            if (newFood >= K) break;
-            else return -1;
-        }
-        for (int i = idx + 2; i < scoville.size(); i++) {
-            if (scoville[i] > newFood) {
-                scoville.insert(scoville.begin() + i, newFood);
-                break;
-            }
-            if (i == scoville.size()-1) {
-                scoville.push_back(newFood);
-                break;
-            }
-        }
-        idx += 2;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < scoville.size(); i++)
+    {
+        pq.push(scoville[i]);
     }
+
+    int first, second, sum;
+    while (pq.top() < K)
+    {
+        first = pq.top();
+        pq.pop();
+        if (pq.empty()) return -1;
+
+        second = pq.top();
+        pq.pop();
+
+        sum = first + second * 2;
+        pq.push(sum);
+        answer++;
+    }
+
     return answer;
 }
